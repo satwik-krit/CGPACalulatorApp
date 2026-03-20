3dce05b56200749026e3ebfc627dcf8dc0219bf1
Satwik Gupta <satwik292007@gmail.com>
2026-03-21 01:24:09 +0530

    Add a basic homepage frontend.

--- app/src/main/java/com/example/cgpacalulator/MainActivity.java
+++ app/src/main/java/com/example/cgpacalulator/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.cgpacalulator;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView tvTotalCredits = findViewById(R.id.tvTotalCredits);
+        TextView tvSemCredits = findViewById(R.id.tvSemCredits);
+
+        tvTotalCredits.setText("23" + " credits");
+        tvSemCredits.setText(String.valueOf(9.17));
     }
 }
